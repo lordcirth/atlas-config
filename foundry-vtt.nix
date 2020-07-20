293a3ea3108a141f7c632a0ee2762f7f0113457c
Nathan Fish <lordcirth@gmail.com>
2020-07-20 21:07:55 +0000

    foundry-vtt: split lines

--- foundry-vtt.nix
+++ foundry-vtt.nix
@@ -1,4 +1,6 @@
-{ stdenv, autoPatchelfHook, unzip, libX11, libXcomposite, glib, libXcursor, libXdamage, libXext, libXi, libXrender, libXtst, libxcb, nspr, dbus, gdk-pixbuf, gtk3, pango, atk, cairo, expat, libXrandr, libXScrnSaver, alsaLib, at-spi2-core, cups, nss, ... }:
+{ stdenv, autoPatchelfHook, unzip, libX11, libXcomposite, glib, libXcursor, libXdamage, libXext, libXi, libXrender,
+  libXtst, libxcb, nspr, dbus, gdk-pixbuf, gtk3, pango, atk, cairo, expat, libXrandr, libXScrnSaver, alsaLib,
+  at-spi2-core, cups, nss, ... }:
 
 let
   zipfile = /nix/store/1jvm0siryg45p8vjdzs7vgzxls50jx31-foundryvtt-0.6.5.zip;
@@ -9,7 +11,10 @@ in
     version = "0.6.5";
     src = zipfile;
 
-    buildInputs = [ autoPatchelfHook unzip libX11 libXcomposite glib libXcursor libXdamage libXext libXrender libXi libXrender libXtst libxcb nspr dbus gdk-pixbuf gtk3 pango atk cairo expat libXrandr libXScrnSaver alsaLib at-spi2-core cups nss ];
+    buildInputs = [ autoPatchelfHook unzip libX11 libXcomposite glib libXcursor libXdamage libXext libXrender libXi
+      libXrender libXtst libxcb nspr dbus gdk-pixbuf gtk3 pango atk cairo expat libXrandr libXScrnSaver alsaLib
+      at-spi2-core cups nss ];
+
     libPath = stdenv.lib.makeLibraryPath [stdenv.cc.cc];
 
     unpackPhase = ''
