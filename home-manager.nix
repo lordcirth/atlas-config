57b9a7937f8b0d5e52296f939984b633cb2a67b6
Nathan Fish <lordcirth@gmail.com>
2020-08-10 04:01:32 +0000

    home-manager: basic neovim config

---
+++ home-manager.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+let 
+  cfg = {
+
+    programs.neovim = {
+      enable = true;
+      viAlias = true;
+      extraConfig = ''
+        set noexpandtab
+        set tabstop=2
+        set shiftwidth=2
+      '';
+    };
+  };
+in
+{
+  imports = [ <home-manager/nixos> ];
+  home-manager.users = {
+    lordcirth = cfg;
+  };
+}
+
